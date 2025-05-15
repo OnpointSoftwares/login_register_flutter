0f2a39515ca8624230f7514822253091fb71a110
OnpointSoftwares <vincentbettoh@gmail.com>
2025-05-15 15:11:34 +0300

    Login layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -42,26 +43,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.indigo,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+           const Image(image: AssetImage('assets/images/logo.png')),
+           const Text('Login',style:TextStyle(color:Colors.white)),
+           const SizedBox(height:20),
+           Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(5)),
             ),
+            child: Column(children:<Widget>[
+              TextField(decoration:InputDecoration(hintText:'Email',)),
+              SizedBox(height:20),
+              TextField(decoration:InputDecoration(hintText:'Password',)),
+            ]),
+           )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
